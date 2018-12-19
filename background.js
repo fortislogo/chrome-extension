abf4cb7898057a4b71c42388f095e62cccea4616
Fortislogo <fortislogo@hotmail.com>
2018-12-19 15:13:34 +0800

    fix login popup

--- background.js
+++ background.js
@@ -2,26 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
+"use strict";
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
+  chrome.storage.sync.set({ color: "#3aa757" }, function() {
     console.log("The color is green.");
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
+            pageUrl: { hostEquals: "developer.chrome.com" }
           }),
-          new chrome.declarativeContent.PageStateMatcher(
-          {
-            pageUrl: {hostEquals: 'bitk.local'},
-            pageUrl: {hostEquals: 'phpstack-93963-566910.cloudwaysapps.com'}
-          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "phpstack-93963-566910.cloudwaysapps.com" }
+          })
         ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
 });
-
--- js/script.js
+++ js/script.js
@@ -1,102 +1,83 @@
-var url = 'http://phpstack-93963-566910.cloudwaysapps.com/chrome/';
+var url = "http://phpstack-93963-566910.cloudwaysapps.com/chrome/";
 //var url = 'http://bitk.locak/chrome/';
 
-function loadContent()
-{
-	$.ajaxSetup(
-	{
-	    headers: 
-	    {
-	    	'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-	    }
-    });
+function loadContent() {
+  $.ajaxSetup({
+    headers: {
+      "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+    }
+  });
 
-	$.ajax(
-	{
-		url: url,
-		type: 'post',
-		success: function(data)
-		{
-			$('.main-container').html(data.html);
-		}
-	});
+  $.ajax({
+    url: url,
+    type: "post",
+    success: function(data) {
+      $(".main-container").html(data.html);
+    }
+  });
 }
 
-$(document).on('submit', '#loginForm', function()
-{
-	var data = $('#loginForm').serialize();	
+$(document).on("submit", "#loginForm", function() {
+  var data = $("#loginForm").serialize();
 
-	$.ajaxSetup(
-	{
-	    headers: 
-	    {
-	    	'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-	    }
-    });
+  $.ajaxSetup({
+    headers: {
+      "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
+    }
+  });
 
-	$.ajax(
-	{
-		url: url + 'login',
-		type: 'post',
-		data:data,
-		success: function(data)
-		{
-			alert(data.html);
-			$('.main-container').html(data.html);
-		}
-	});
+  $.ajax({
+    url: url + "login",
+    type: "post",
+    data: data,
+    success: function(data) {
+      $(".main-container").html(data.html);
+    }
+  });
 
-	return false;
-
+  return false;
 });
 
-
-$(document).ready(function()
-{
-	loadContent();
-
+$(document).ready(function() {
+  loadContent();
 });
 
 function equal_heights($) {
+  $(".stack-links .single-link .title").matchHeight({
+    byRow: false,
+    property: "height"
+  });
 
-	$('.stack-links .single-link .title').matchHeight({
-		byRow: false,
-	    property: 'height'
-	});
+  // Dashboard Tiles
+  $(".dash-stack-tile").matchHeight({
+    byRow: false,
+    property: "height"
+  });
 
-	// Dashboard Tiles
-	$('.dash-stack-tile').matchHeight({
-		byRow: false,
-	    property: 'height'
-	});
-
-	/*
+  /*
 	$('.dash-stack-tile > .stack-content > .title').matchHeight({
 		byRow: false,
 	    property: 'height'
 	});
 	*/
 
-	//Dashboard People
-	$('.stack-panel-people .people-box').matchHeight({
-		byRow: false,
-	    property: 'height'
-	});
+  //Dashboard People
+  $(".stack-panel-people .people-box").matchHeight({
+    byRow: false,
+    property: "height"
+  });
 
-	//People Page
-	$('.people-page .people-box').matchHeight({
-		byRow: false,
-	    property: 'height'
-	});
-
+  //People Page
+  $(".people-page .people-box").matchHeight({
+    byRow: false,
+    property: "height"
+  });
 }
 
 function dashboard_controls($) {
-
-	// Dashboard Layout Controls
-	$('.layout-control > .btn-control').on('click', function() {
-		$('.layout-control > .btn-control').removeClass('active');
-		$(this).addClass('active');
-	});
-
+  // Dashboard Layout Controls
+  $(".layout-control > .btn-control").on("click", function() {
+    $(".layout-control > .btn-control").removeClass("active");
+    $(this).addClass("active");
+  });
 }
